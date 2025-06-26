853b6d79b8b21d953e17febb8a597a81be0421f7
rajdeep1113 <rajdeepsing7568@gmail.com>
2025-06-26 17:28:53 +0530

    form

---
+++ Form/layout/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './componet/Footer'
+import Section from './componet/Section'
+import Header from './componet/Header'
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Section/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
